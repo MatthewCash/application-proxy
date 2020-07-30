942bf3c9be11b1b2299b7d61c0e6ad2896061a17
Matthew_Cash <matthew@matthew-cash.com>
2020-07-30 02:04:18 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# application-proxy
+
+Simple NodeJS HTTPS proxy
+
+I use this proxy in my web applications to proxy different routes to different servers
---
+++ main.ts
@@ -0,0 +1,66 @@
+import spdy from 'spdy';
+import { promises as fs } from 'fs';
+import { Agent, request } from 'https';
+
+const localAgent = new Agent({
+    rejectUnauthorized: false
+});
+
+interface Server {
+    host: string;
+    port: number;
+    path: string;
+}
+
+const mainServer: Server = {
+    host: '127.0.0.1',
+    port: 444,
+    path: ''
+};
+
+const apiServer: Server = {
+    host: '127.0.0.1',
+    port: 446,
+    path: '/api'
+};
+
+const servers: Server[] = [mainServer, apiServer];
+
+const app = async (clientReq, clientRes) => {
+    const tryServers = [...servers].reverse();
+    tryServers.some(server => {
+        if (clientReq.url.indexOf(server.path) !== 0) return false;
+
+        const proxyReq = request(
+            {
+                hostname: '127.0.0.1',
+                port: 446,
+                path: clientReq.url.substring(server.path.length),
+                method: clientReq.method,
+                headers: clientReq.headers,
+                agent: localAgent
+            },
+            async res => {
+                clientRes.writeHead(res.statusCode, res.headers);
+                res.pipe(clientRes, { end: true });
+            }
+        );
+
+        clientReq.pipe(proxyReq, { end: true });
+        return true;
+    });
+};
+
+const host = '127.0.0.1';
+const port = 443;
+
+const main = async () => {
+    const key = await fs.readFile('./server.key');
+    const cert = await fs.readFile('./server.crt');
+
+    spdy.createServer({ key, cert }, app).listen(port, host);
+
+    console.log(`[Ready] Listening on ${host}:${port}`);
+};
+
+main();
